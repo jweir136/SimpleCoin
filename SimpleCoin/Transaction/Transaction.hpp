45dca75ab901f64b6b3bf6c6c579c06f0b2c5276
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-07-07 23:03:17 -0400

    add TxIn

--- SimpleCoin/Transaction/Transaction.hpp
+++ SimpleCoin/Transaction/Transaction.hpp
@@ -9,9 +9,40 @@
 #include <stdexcept>
 #include <ctime>
 #include <regex>
+#include <include/json/json.hpp>
+
+using namespace nlohmann;
 
 namespace Tx {
+    class TxIn {
+        public:
+            unsigned int    amount;
+            unsigned long   block;
+            unsigned long   tx;
+            json      json_string;
 
+            TxIn(unsigned int amount, unsigned long block, unsigned long tx) {
+                this->amount = amount;
+                this->block = block;
+                this->tx = tx;
+
+                this->json_string["amount"] = amount;
+                this->json_string["block"] = block;
+                this->json_string["tx"] = tx;
+            }
+
+            TxIn(std::string json_string) {
+                this->json_string = json::parse(json_string);
+
+                this->amount = this->json_string["amount"];
+                this->block = this->json_string["block"];
+                this->tx = this->json_string["tx"];
+            }
+
+            std::string json() {
+                return this->json_string.dump();
+            }
+    };
 }
 
 #endif
--- test/TxIn.test.cpp
+++ test/TxIn.test.cpp
@@ -4,3 +4,28 @@
 #include <ctime>
 #include <string>
 
+TEST_CASE("txin", "[]") {
+    srand(time(NULL));
+    unsigned int amount;
+    unsigned long block;
+    unsigned long tx;
+    Tx::TxIn* txin;
+
+    for (int i = 0; i < 100; i++) {
+        amount = rand() % 10000;
+        block = rand() % 100000;
+        tx = rand() % 10000;
+
+        Tx::TxIn* txin;
+        txin = new Tx::TxIn(amount, block, tx);
+        Tx::TxIn* newtxin;
+        newtxin = new Tx::TxIn(txin->json());
+
+        REQUIRE(txin->amount == newtxin->amount);
+        REQUIRE(txin->block == newtxin->block);
+        REQUIRE(txin->tx == newtxin->tx);
+
+        delete newtxin;
+        delete txin;
+    }
+}
