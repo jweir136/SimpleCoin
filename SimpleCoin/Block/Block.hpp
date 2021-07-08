03d31e79fd380a1379b9615117170b9a6e5fb6cf
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-07-08 17:00:59 -0400

    add Block class

---
+++ SimpleCoin/Block/Block.hpp
@@ -0,0 +1,69 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <functional>
+#include <include/json/json.hpp>
+#include <SimpleCoin/Transaction/Transaction.hpp>
+
+using namespace nlohmann;
+
+    class Block {
+        public:
+            unsigned long       epoch;
+            json                transactions;
+            std::size_t         hash;
+            unsigned long long  nonce;
+            bool                empty;
+            json                json_string;
+
+            Block() {
+                this->epoch = time(NULL);
+                this->transactions = {};
+                this->hash = 0;
+                this->nonce = 0;
+                this->empty = true;
+                
+                this->json_string["time"] = this->epoch;
+                this->json_string["hash"] = this->hash;
+                this->json_string["nonce"] = this->nonce;
+                this->json_string["transactions"] = this->transactions;
+            }
+
+            Block(std::string json_string) {
+                this->json_string = json::parse(json_string);
+                this->empty = false;
+                
+                this->epoch = this->json_string["time"];
+                this->hash = this->json_string["hash"];
+                this->nonce = this->json_string["nonce"];
+                this->transactions = this->json_string["transactions"];
+            }
+
+            void add_transaction(std::string transaction_json) {
+                this->json_string["transactions"].push_back(transaction_json);
+                this->hash = compute_hash();
+                this->json_string["hash"] = this->hash;
+            }
+
+            std::string json() {
+                this->json_string["hash"] = compute_hash();
+
+                return this->json_string.dump();
+            }
+    
+        private:
+            std::size_t compute_hash() {
+                std::string epoch_string = std::to_string(this->epoch);
+                std::string nonce_string = std::to_string(this->nonce);
+                std::string trans_hash = this->json_string["transactions"].dump();
+
+                return std::hash<std::string>()(epoch_string + nonce_string + trans_hash);
+            }
+    };
+
+
+#endif
---
+++ test/Block.test.cpp
@@ -0,0 +1,50 @@
+#include <include/catch2/catch.hpp>
+#include <SimpleCoin/Block/Block.hpp>
+#include <SimpleCoin/Transaction/Transaction.hpp>
+#include <iostream>
+#include <ctime>
+#include <string>
+
+TEST_CASE("block", "[]") {
+    srand(time(NULL));
+    unsigned int amount;
+    unsigned long reciever;
+
+    Tx::TxOuts* txouts = new Tx::TxOuts();
+
+    unsigned long block;
+    unsigned long tx;
+
+    Tx::TxIns* txins = new Tx::TxIns();
+
+    Tx::Transaction* trans;
+    Block* blockobj = new Block();
+    Block* blockobj2;
+
+    for (int i = 0; i < 100; i++) {
+        amount = rand() % 1000;
+        reciever = rand() % 1000;
+
+        txouts->add_txout(amount, reciever);
+
+        block = rand() % 1000;
+        tx = rand() % 1000;
+
+        txins->add_txin(amount, block, tx);
+
+        trans = new Tx::Transaction(txins->json(), txouts->json());
+        blockobj->add_transaction(trans->json());
+
+        blockobj2 = new Block(blockobj->json());
+
+        REQUIRE(blockobj->epoch == blockobj2->epoch);
+        REQUIRE(blockobj->hash == blockobj2->hash);
+        REQUIRE(blockobj->nonce == blockobj2->nonce);
+    }
+
+    delete txins;
+    delete txouts;
+    delete trans;
+    delete blockobj;
+    delete blockobj2;
+};
